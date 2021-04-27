e0ec69e2bab3164ae9df1f2dac142feaad48bbb9
Patricia <patricia.bildea@gmail.com>
2021-04-27 20:55:59 +0300

    Update book feature

--- ITSchoolBookApp.py
+++ ITSchoolBookApp.py
@@ -17,11 +17,36 @@ def list_books():
         index = 0
         for title in list_of_books:
             index += 1
-            print(f'''{index}. {title["BookTitle"]} by {title["BookAuthor"]}. Shared with: {title["SharedWith"]}. Book read: {title["IsRead"]}''')
+            print(f'{index}. {title["BookTitle"]} by {title["BookAuthor"]}. Shared with: {title["SharedWith"]}. Book read: {title["IsRead"]}')
 
 
 def update_book():
-    print("Update a book")
+    title = input("What book are you looking for today? --> ")
+    list_of_books = []
+    read = "No"
+    import csv
+    with open("BooksDB.csv", mode="r") as file:
+        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+        for book in list_of_books:
+            if book.get("BookTitle") != title:
+                add = input('''This book is not in our list.
+Would you like to add this book? Y/N --> ''')
+                if add == "Y":
+                    add_book()
+                else:
+                    print("Good bye!")
+            else:
+                read = input(f"Did you read {title}? Y/N --> ")
+                if read == "Y":
+                    read = "Yes"
+                    with open("BooksDB.csv", mode="w") as file:
+                        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+                        writer.writerow({"BookTitle": book.get("BookTitle"), "BookAuthor": book.get("BookAuthor"),
+"SharedWith": book.get("SharedWith"), "IsRead": read})
+                    print(f"Book {title} was updated successfully!")
+                break
+
+
 
 
 def share_book():
