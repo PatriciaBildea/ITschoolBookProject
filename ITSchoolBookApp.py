7bdfbb19f119c45a6075c02c4830bec7a60de062
Patricia <patricia.bildea@gmail.com>
2021-05-04 02:38:31 +0300

    Added review feature with condition that bok has to be read to be able to review, added loop to menu, improved share option to be able to share with multiple users, improved title input to be upper case for more accurate returns and book author upper for consistency, added some filters for incorrect inputs, added improved add feature to see if book is already in the list, fixed bug that stopped searching after first row

--- ITSchoolBookApp.py
+++ ITSchoolBookApp.py
@@ -1,97 +1,188 @@
 def add_book():
-    book_title = input("Add book title --> ")
-    book_author = input("Add book author --> ")
+    book_title = input("Add book title --> ").upper()
 
     import csv  # importing csv library
-    with open("BooksDB.csv", mode="a") as file:
-        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
-        writer.writerow({"BookTitle": book_title, "BookAuthor": book_author, "SharedWith": "Nobody", "IsRead": "No"})
-    print(f"Book '{book_title}' by {book_author} was added successfully!")
+    with open("BooksDB.csv", mode="r") as file:
+        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
+        book_not_present = True
+        for book in list_of_books:
+            if book.get("BookTitle") == book_title:
+                book_not_present = False
+        if book_not_present:
+            book_author = input("Add book author --> ").upper()
+            with open("BooksDB.csv", mode="a") as file:
+                list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
+                writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
+                writer.writerow({"BookTitle": book_title, "BookAuthor": book_author, "SharedWith": "", "IsRead": "Not read", "Review": "No review added"})
+            print(f"Book '{book_title}' by {book_author} was added successfully!")
+        else:
+            print("Book already added")
 
 
 def list_books():
     import csv
     with open("BooksDB.csv", mode="r") as file:
-        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])  # takes all from DB
+        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])  # takes all from DB
         print("Available books are: ")
         index = 0
         for title in list_of_books:
             index += 1
-            print(f'{index}. {title["BookTitle"]} by {title["BookAuthor"]}. Shared with: {title["SharedWith"]}. Book read: {title["IsRead"]}')
+            print(f'{index}. {title["BookTitle"]} by {title["BookAuthor"]}. Shared with: {title["SharedWith"]}. Book read: {title["IsRead"]}. Review: {title["Review"]}')
 
 
 def update_book():
-    title = input("What book are you looking for today? --> ")
+    title = input("What book are you looking for today? --> ").upper()
     list_of_books = []
-    read = "No"
+    read = "Not read"
     import csv
     with open("BooksDB.csv", mode="r") as file:
-        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
         for book in list_of_books:
-            if book.get("BookTitle") != title:
-                add = input('''This book is not in our list.
-Would you like to add this book? Y/N --> ''')
-                if add == "Y":
-                    add_book()
-                else:
-                    print("Good bye!")
-            else:
-                read = input(f"Did you read {title}? Y/N --> ")
-                if read == "Y" or read == "y":
-                    read = "Yes"
+            if book.get("BookTitle") == title:
+                book_not_present = False
+                while wrong_choice:
+                    read = input(f"Did you read {title}? Y/N --> ")
+                    if read not in ["y", "Y", "n", "N"]:
+                        wrong_choice = True
+                        print("That is not a valid option")
+                    else:
+                        wrong_choice = False
+                if read == "Y" or "y":
+                    read = "Read"
                     with open("BooksDB.csv", mode="w") as file:
-                        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+                        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
                         writer.writerow({"BookTitle": book.get("BookTitle"), "BookAuthor": book.get("BookAuthor"),
-"SharedWith": book.get("SharedWith"), "IsRead": read})
+"SharedWith": book.get("SharedWith"), "IsRead": read, "Review": book.get("Review")})
                 else:
-                    read = "No"
+                    read = "Not read"
                     with open("BooksDB.csv", mode="w") as file:
-                        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+                        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
                         writer.writerow({"BookTitle": book.get("BookTitle"), "BookAuthor": book.get("BookAuthor"),
-                                         "SharedWith": book.get("SharedWith"), "IsRead": read})
+                                         "SharedWith": book.get("SharedWith"), "IsRead": read, "Review": book.get("Review")})
                 print(f"Book {title} was updated successfully!")
-            break
-
+                break
+            else:
+                book_not_present = True
+    if book_not_present:
+        while wrong_choice:
+            add = input('''This book is not in our list. Would you like to add this book? Y/N --> ''')
+            if add not in ["y", "Y", "n", "N"]:
+                wrong_choice = True
+                print("That is not a valid option")
+            else:
+                wrong_choice = False
+        if add == "Y" or "y":
+            add_book()
 
 def share_book():
-    title = input("What book are you looking for today? --> ")
+    title = input("What book are you looking for today? --> ").upper()
     list_of_books = []
-    shared_with = "None"
     import csv
     with open("BooksDB.csv", mode="r") as file:
-        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
         for book in list_of_books:
-            if book.get("BookTitle") != title:
-                add = input('''This book is not in our list.
-Would you like to add this book? Y/N --> ''')
-                if add == "Y":
-                    add_book()
-                else:
-                    print("Good bye!")
-            else:
-                shared_with = input(f"Who would you like to share {title} with? --> ")
+            if book.get("BookTitle") == title:
+                new_share = input(f"Who would you like to share {title} with? --> ")
+                shared_with = book.get("SharedWith") + ", " + new_share
                 with open("BooksDB.csv", mode="w") as file:
-                    writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead"])
+                    writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
                     writer.writerow({"BookTitle": book.get("BookTitle"), "BookAuthor": book.get("BookAuthor"),
-                                     "SharedWith": shared_with, "IsRead": book.get("IsRead")})
+                                     "SharedWith": shared_with, "IsRead": book.get("IsRead"), "Review": book.get("Review")})
                 print(f"{title} is now shared with {shared_with}")
+                book_not_present = False
+                break
+            else:
+                book_not_present = True
+    if book_not_present:
+        while wrong_choice:
+            add = input('''This book is not in our list. Would you like to add this book? Y/N --> ''')
+            if add not in ["y", "Y", "n", "N"]:
+                wrong_choice = True
+                print("That is not a valid option")
+            else:
+                wrong_choice = False
+        if add == "Y" or "y":
+            add_book()
 
 
+def book_review():
+    title = input("What book are you looking for today? --> ").upper()
+    list_of_books = []
+    import csv
+    with open("BooksDB.csv", mode="r") as file:
+        list_of_books = csv.DictReader(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
+        for book in list_of_books:
+            if book.get("BookTitle") == title:
+                book_not_present = False
+                if book.get("IsRead") == "Read":
+                    review = input(f"What did you think of {title} --> ")
+                    with open("BooksDB.csv", mode="w") as file:
+                        writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead", "Review"])
+                        writer.writerow({"BookTitle": book.get("BookTitle"), "BookAuthor": book.get("BookAuthor"),
+                                         "SharedWith": book.get("SharedWith"), "IsRead": book.get("IsRead"), "Review": review})
+                    print(f"Review successfully added for {title}")
+                else:
+                    wrong_choice = True
+                    while wrong_choice:
+                        read = input(f"Did you read {title}? Y/N --> ")
+                        if read not in ["y", "Y", "n", "N"]:
+                            wrong_choice = True
+                            print("That is not a valid option")
+                        else:
+                            wrong_choice = False
+                    if read == "Y" or "y":
+                        read = "Read"
+                        with open("BooksDB.csv", mode="w") as file:
+                            writer = csv.DictWriter(file, fieldnames=["BookTitle", "BookAuthor", "SharedWith", "IsRead",
+                                                                      "Review"])
+                            writer.writerow({"BookTitle": book.get("BookTitle"), "BookAuthor": book.get("BookAuthor"),
+                                             "SharedWith": book.get("SharedWith"), "IsRead": read,
+                                             "Review": book.get("Review")})
+                    else:
+                        print("You can't leave a review for a book that is not read. Please select a different option")
+                break
+            else:
+                book_not_present = True
+    if book_not_present:
+        wrong_choice = True
+        while wrong_choice:
+            add = input('''This book is not in our list. Would you like to add this book? Y/N --> ''')
+            if add not in ["y", "Y", "n", "N"]:
+                wrong_choice = True
+                print("That is not a valid option")
+            else:
+                wrong_choice = False
+        if add == "Y" or "y":
+            add_book()
+
+
+book_not_present = True
+add = ""
+wrong_choice = True
+menu = True
 # Main menu for user
-option = int(input('''Menu:
-1: Add a book
-2: List books
-3: Update a book
-4: Share a book
-Select one option --> '''))
+while menu:
+    option = input('''Menu:
+    1: Add a book
+    2: List books
+    3: Update a book
+    4: Share a book
+    5: Add book review
+    6: Exit
+    Select one option --> ''')
 
-if option == 1:
-    add_book()
-elif option == 2:
-    list_books()
-elif option == 3:
-    update_book()
-elif option == 4:
-    share_book()
-else:
-    print("That is not a valid option")
+    if option not in ["1", "2", "3", "4", "5", "6"]:
+        print("That is not a valid option")
+    elif int(option) == 1:
+        add_book()
+    elif int(option) == 2:
+        list_books()
+    elif int(option) == 3:
+        update_book()
+    elif int(option) == 4:
+        share_book()
+    elif int(option) == 5:
+        book_review()
+    elif int(option) == 6:
+        menu = False
+        print("Good bye!")
